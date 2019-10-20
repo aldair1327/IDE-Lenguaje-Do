57582e99c5777d9bed2559c7cc64f936372b251f
Williams Vallejo <wibrvallejoma@ittepic.edu.mx>
2019-10-20 12:57:14 -0600

    Adicion del archivo para los AFN

---
+++ src/Analisis_Lexico/Automata_finito/AutomataFinitoN.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analisis_Lexico.Automata_finito;
+
+import Analisis_Lexico.Token_;
+import java.util.Stack;
+
+/**
+ *
+ * @author Williams Vallejo
+ */
+public class AutomatasFinitosN {
+    private Stack<Token_> pila;
+    
+    AutomatasFinitosN(Stack<Token_> pila){
+        this.pila = pila;
+    }
+    
+    
+}
