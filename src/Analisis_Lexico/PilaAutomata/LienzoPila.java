d2ee62b029f39098cb475b886ccfca51a9438c89
Gabriela Patricia <gapadelarosagu@ittepic.edu.mx>
2019-11-16 20:54:24 -0600

    grafica pila incorrecta

--- src/Analisis_Lexico/PilaAutomata/LienzoPila.java
+++ src/Analisis_Lexico/PilaAutomata/LienzoPila.java
@@ -23,18 +23,35 @@ public class LienzoPila extends JPanel{
     public int alto_ventana = 0;
     private int max;
     private int general;
+    private boolean valid;
+    private int tope;
 
-    public LienzoPila(int ancho, int alto,ArrayList<String> automata_modelo, ArrayList<Token_> automata_a_validar){
+    public LienzoPila(int ancho, int alto,ArrayList<String> automata_modelo, ArrayList<Token_> automata_a_validar,boolean valid,int tope){
         this.ancho_ventana = ancho;
         this.alto_ventana = alto;
         this.automata_modelo = automata_modelo;
         this.automata_a_validar = automata_a_validar;
         this.max=automata_a_validar.size();
         this.general=0;
-        for(Token_ elemento : automata_a_validar){
-            cadena_automata += " " + elemento.lexema ;
-            aux.add(elemento);
-            aux2.add(elemento);
+        this.valid=valid;
+        this.tope=tope;
+        if (valid) {
+            for(Token_ elemento : automata_a_validar){
+                cadena_automata += " " + elemento.lexema ;
+                aux.add(elemento);
+                aux2.add(elemento);
+            }
+        }
+        else {
+            for(Token_ elemento : automata_a_validar){
+                cadena_automata += " " + elemento.lexema ;
+                aux.add(elemento);
+                aux2.add(elemento);
+            }
+            /*for(int i=0;i<=tope;i++){
+                aux.add(automata_a_validar.get(i));
+                aux2.add(automata_a_validar.get(i));
+            }*/
         }
         //Collections.reverse(aux);
     }
@@ -51,33 +68,57 @@ public class LienzoPila extends JPanel{
 
     @Override
     public void paintComponent(Graphics g) {
-        //super.repaint();
-        //super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
         fondo(g);
         //Ecuacion inicial
         textoEcuacion(g,"Instruccion inicial: " + cadena_automata );
+        if (!valid) {
+            g.setColor(new Color(149,173,208));
+            g.setFont(new Font("Arial", 1, 10));
+            String aux="Caso: TIPO_DATO";
+            for (String element: automata_modelo) {
+                aux+=" " + element;
+            }
+            g.drawString(aux, ( this.ancho_ventana / 2 ) - 125, 75);
+        }
         this.aux=this.aux2;
         this.general=0;
         //Estado inicial
         g.setFont(new Font("Arial", 0, 12));
         g.setColor(Color.BLACK);
-        //g.drawLine(10, this.alto_ventana / 2, 30, this.alto_ventana / 2);
-        for (int i = 1; i <= aux2.size() ; i++) {
-            if (i < aux2.size()  ) {
-                dibujarTransicion(g, ((160 * i)-30), (30+ (160 * i)));
-                g.drawLine((30+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)-15);
-                g.drawLine((30+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)+15);
-                //letraTransicion(g, (80 + (130 * i)), automata_modelo.get(i));
-            }else{
-                dibujarTransicion(g, ((160 * i)-30), ((160 * i)));
-                g.drawLine(( (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (-15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)-15);
-                g.drawLine(((160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (-15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)+15);
-                g.setColor(Color.GREEN);
-                g.drawString("Pila", ((160 * i))+15, (this.alto_ventana / 3)+(this.alto_ventana / 6)-15);
-                g.drawString("Validada", ((160 * i))+5, (this.alto_ventana / 3)+(this.alto_ventana / 6)+10);
+        if (valid) {
+            for (int i = 1; i <= aux2.size() ; i++) {
+                if (i < aux2.size()  ) {
+                    dibujarTransicion(g, ((160 * i)-30), (30+ (160 * i)));
+                    g.drawLine((30+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)-15);
+                    g.drawLine((30+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)+15);
+                }else{
+                    dibujarTransicion(g, ((160 * i)-30), ((160 * i)));
+                    g.drawLine(( (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (-15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)-15);
+                    g.drawLine(((160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (-15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)+15);
+                    g.setColor((valid)?Color.GREEN:Color.RED);
+                    g.drawString("Pila", ((160 * i))+15, (this.alto_ventana / 3)+(this.alto_ventana / 6)-15);
+                    g.drawString((valid)?"Validada":"Invalida", ((160 * i))+5, (this.alto_ventana / 3)+(this.alto_ventana / 6)+10);
+                }
+                dibujarPila(g,30 + (160 * (i-1)),(this.alto_ventana / 3)-20);
+                this.general++;
             }
-            dibujarPila(g,30 + (160 * (i-1)),(this.alto_ventana / 3));
-            this.general++;
+        } else {
+            for (int i = 1; i <= tope+1 ; i++) {
+                if (i <= tope  ) {
+                    dibujarTransicion(g, ((160 * i)-30), (30+ (160 * i)));
+                    g.drawLine((30+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)-15);
+                    g.drawLine((30+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)+15);
+                }else{
+                    dibujarTransicion(g, ((160 * i)-30), ((160 * i)));
+                    g.drawLine(( (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (-15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)-15);
+                    g.drawLine(((160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6), (-15+ (160 * i)), (this.alto_ventana / 3)+(this.alto_ventana / 6)+15);
+                    g.setColor((valid)?Color.GREEN:Color.RED);
+                    g.drawString("Pila", ((160 * i))+15, (this.alto_ventana / 3)+(this.alto_ventana / 6)-15);
+                    g.drawString((valid)?"Validada":"Invalida", ((160 * i))+5, (this.alto_ventana / 3)+(this.alto_ventana / 6)+10);
+                }
+                dibujarPila(g,30 + (160 * (i-1)),(this.alto_ventana / 3)-20);
+                this.general++;
+            }
         }
     }
     
@@ -101,10 +142,18 @@ public class LienzoPila extends JPanel{
         g.drawLine(x+100, y+this.max*30, x+100, y);
         int auxY=y+this.max*30;
         if (aux!=null) {
-            for (int i=this.aux.size()-1;i>=this.general;i--) {
-                g.drawRect(x, auxY, 100, 30);
-                g.drawString(aux.get(i).lexema, x+10, auxY+20);
-                auxY-=30;
+            if (valid) {
+                for (int i=this.aux.size()-1;i>=this.general;i--) {
+                    g.drawRect(x, auxY, 100, 30);
+                    g.drawString(aux.get(i).lexema, x+10, auxY+20);
+                    auxY-=30;
+                }
+            } else {
+                for (int i=this.aux.size()-1;i>=this.general && this.general<=this.tope;i--) {
+                    g.drawRect(x, auxY, 100, 30);
+                    g.drawString(aux.get(i).lexema, x+10, auxY+20);
+                    auxY-=30;
+                }
             }
             
         }
--- src/Analisis_Lexico/PilaAutomata/ValidacionPila.java
+++ src/Analisis_Lexico/PilaAutomata/ValidacionPila.java
@@ -7,19 +7,23 @@ package Analisis_Lexico.PilaAutomata;
 
 import Analisis_Lexico.Token_;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import javax.swing.JTabbedPane;
 /**
  *
  * @author paty6
  */
 public class ValidacionPila {
+    private int tope;
     public ArrayList<Token_> automata;
     
     public ValidacionPila(ArrayList<Token_> automata){
+        this.tope=0;
         this.automata = automata;
     }
     
-    public LienzoPila genararAutomata(){
+    public JTabbedPane genararAutomata(){
         
         switch(automata.get(0).componente_lexico){
             case "ENTERO":
@@ -32,10 +36,10 @@ public class ValidacionPila {
         return null;
     }
     
-    private LienzoPila afn_declaracion(ArrayList<Token_> automata_temporal){
+    private JTabbedPane afn_declaracion(ArrayList<Token_> automata_temporal){
         //Generacion de pila de los posibles resultados
         if (automata_temporal.isEmpty()) {
-            return null;
+            return new JTabbedPane();
         }
         Token_ tipo_dato = automata_temporal.get(0);
         
@@ -56,58 +60,68 @@ public class ValidacionPila {
         //automata_temporal.remove(0);
         
         //Empieza el proceso de comparacion
-        
-        System.out.println("IF 1 Pila******************************");
+        JTabbedPane stacks=new JTabbedPane();
+        //System.out.println("IF 1 Pila******************************");
         if (validacion(automata_temporal.subList(1, automata_temporal.size()), automata_declaracion_a)) {
-            LienzoPila t = new LienzoPila(900, 500,automata_declaracion_a,automata_temporal);
+            LienzoPila t = new LienzoPila(900, 500,automata_declaracion_a,automata_temporal,true,0);
             //t.setAutomata(automata_declaracion_a, automata_temporal);
             //return "Eligió el automata de declaracion 1";
-            return t;
+            stacks.addTab("Correcta", t);
+            return stacks;
         }
+        LienzoPila a = new LienzoPila(900, 500,automata_declaracion_a,automata_temporal,false,tope);
+        stacks.addTab("Incorrecta", a);
+        tope=0;
         //System.out.println("Automata de Declaracion 1 Invalido\nIF 2*****************************");
         if (validacion(automata_temporal.subList(1, automata_temporal.size()), automata_declaracion_b)) {  
             //return "Eligió el automata de declaracion 2";
-            LienzoPila t = new LienzoPila(900, 500,automata_declaracion_b,automata_temporal);
+            LienzoPila t = new LienzoPila(900, 500,automata_declaracion_b,automata_temporal,true,0);
             //t.setAutomata(automata_declaracion_b, automata_temporal);
-            return t;
+            stacks.addTab("Correcta", t);
+            return stacks;
         }
+        LienzoPila b = new LienzoPila(900, 500,automata_declaracion_b,automata_temporal,false,tope);
+        stacks.addTab("Incorrecta",b);
+        tope=0;
         //System.out.println("Automata de Declaracion 2 Invalido\nIF 3****************************");
         if (validacion(automata_temporal.subList(1, automata_temporal.size()), automata_declaracion_c)) {           
             //return "Eligió el automata de declaracion 3";
             //t.setAutomata(automata_declaracion_c, automata_temporal);
-            LienzoPila t = new LienzoPila(900, 500,automata_declaracion_c,automata_temporal);
-            return t;
+            LienzoPila t = new LienzoPila(900, 500,automata_declaracion_c,automata_temporal,true,0);
+            stacks.addTab("Correcta", t);
+            return stacks;
         }
         //System.out.println("Automata de Declaracion 3 Invalido\nIF 4****************************");
+        LienzoPila c = new LienzoPila(900, 500,automata_declaracion_c,automata_temporal,false,tope);
+        stacks.addTab("Incorrecta",c);
+        tope=0;
         if (validacion(automata_temporal.subList(1, automata_temporal.size()), automata_declaracion_d)) {
             //return "Eligió el automata de declaracion 4";
             //t.setAutomata(automata_declaracion_d, automata_temporal);
-            LienzoPila t = new LienzoPila(900, 500,automata_declaracion_d,automata_temporal);
-            return t;
+            LienzoPila t = new LienzoPila(900, 500,automata_declaracion_d,automata_temporal,true,0);
+            stacks.addTab("Correcta", t);
+            return stacks;
         }
+        LienzoPila d = new LienzoPila(900, 500,automata_declaracion_d,automata_temporal,false,tope);
+        stacks.addTab("Incorrecta",d);
+        tope=0;
         //System.out.println("Automata de Declaracion 4 Invalido\nAutomata Incorrecto");
-        return null;
-        
+        return stacks;
     }
     
-    private LienzoPila afn_asignacion(ArrayList<Token_> automata_temporal){
+    private JTabbedPane afn_asignacion(ArrayList<Token_> automata_temporal){
         //Generacion de pila de los posibles resultados
         if (automata_temporal.size() != 4) {
-            return null;
+            return new JTabbedPane();
         }
         String tipos_asignacion[] = {"ID","PALABRA_RESERVADA", "DO", "RE", "MI", "FA", "SOL", "LA", "SII", "numEntero"};
         String asignacion[] = {"ID", "ASSIGNACION", "VALOR", "PUNTO_Y_COMA"};
-        
         ArrayList<String> automata_asignacion;
-        
         automata_asignacion = llenarLista(asignacion);
-        
         //Empieza el proceso de comparacion
         boolean cambio = false;
-        //System.out.println("Token en 2: " + automata_temporal.get(2).componente_lexico);
-                
+        JTabbedPane stacks=new JTabbedPane();
         for(String tipo: tipos_asignacion){ 
-            
             if(automata_temporal.get(2).componente_lexico.equals(tipo)) {
                 automata_asignacion.set(2, automata_temporal.get(2).componente_lexico);
                 cambio = true;
@@ -128,50 +142,41 @@ public class ValidacionPila {
         }
         cadena2 +="]";
         if (validacion(automata_temporal, automata_asignacion)) {
-            LienzoPila t = new LienzoPila(900, 500,automata_asignacion,automata_temporal);
-            //t.setAutomata(automata_asignacion, automata_temporal);
-            return t;
+            LienzoPila t = new LienzoPila(900, 500,automata_asignacion,automata_temporal,true,0);
+            stacks.addTab("Correcta", t);
+            return stacks;
         }
-
-        return null;
+        LienzoPila t = new LienzoPila(900, 500,automata_asignacion,automata_temporal,false,tope);
+        stacks.addTab("Incorrecta", t);
+        tope=0;
+        return stacks;
     }
     
     private boolean validacion(List<Token_> datos, List<String> declaracion){
-        if (datos.isEmpty()&& declaracion.isEmpty()) {            
-            //System.out.println("No quedan elementos por validar\nPila Valida");
+        if (datos.isEmpty()&& declaracion.isEmpty()){
             return true;
         }
         if (datos.isEmpty()) {
-            //System.out.println("Pila Vacia-Faltan elementos\nPila Invalida");
             return false;
         }
         if (declaracion.isEmpty()) {
-            //System.out.println("Sobran elementos en la validacion\nPila Invalida");
             return false;
         }
-        //System.out.println("--------");
-        List<Token_> temp=datos;        
-        
         boolean comparacion=datos.get(0).componente_lexico.equals(declaracion.get(0));
-        
-        //System.out.println(datos.get(0).componente_lexico+" = "+declaracion.get(0)+" ? "+ (comparacion?"Si":"No"));
-        
         if (comparacion) {
-            //System.out.println("Se remueve "+datos.get(0).componente_lexico);
-            //En este punto se podria agregar otra pila a la vista para que sean redibujadas ambas pilas y pueda ser observado el proceso
+            tope++;
             return(validacion(
                 datos.subList(1, datos.size()), 
                 declaracion.subList(1, declaracion.size())
             ));
         }
-        //System.out.println("Pila Invalida");
         return false;
     }
    
     
     private ArrayList<String> llenarLista(String[]datos){
         ArrayList<String> nuevaLista = new ArrayList<>();
-        for(String elemento: datos) nuevaLista.add(elemento);
+        nuevaLista.addAll(Arrays.asList(datos));
         return nuevaLista;
     }
 }
